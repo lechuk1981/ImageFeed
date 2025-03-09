9667536a2929fdc4f26260c36ec2c6d8c964ae49
Lechuk <lechuk@yandex.ru>
2025-03-09 19:47:18 +0300

    Add ImageList constraints

---
+++ ImageFeed/ImagesListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Andrey Sopov on 04.03.2025.
+//
+
+import UIKit
+
+class ImagesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
