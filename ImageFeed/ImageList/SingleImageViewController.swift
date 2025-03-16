11c43bdce444f2059ac1a9173018d6dfb195dadd
Lechuk <lechuk@yandex.ru>
2025-03-16 23:51:30 +0300

    Added zoom

--- ImageFeed/ImageList/SingleImageViewController.swift
+++ ImageFeed/ImageList/SingleImageViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     var image: UIImage?{
         didSet {
-            guard isViewLoaded else { return }
+            guard isViewLoaded, let image else { return }
             imageView.image = image
+            imageView.frame.size = image.size
         }
     }
     
@@ -24,5 +25,14 @@ final class SingleImageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
