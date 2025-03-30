f92909917e305802db3a3f35316244a4cd6fc8d2
Lechuk <lechuk@yandex.ru>
2025-03-30 21:36:16 +0300

    Added WebViewViewController

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Andrey Sopov on 30.03.2025.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    
+    
+}
