8fc568962c40e9e77d35df433b124de8a20bb11e
Lechuk <lechuk@yandex.ru>
2025-04-13 12:38:21 +0300

    Added SplashViewController

---
+++ ImageFeed/Auth/OAuth2Service.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  ImageFeed
+//
+//  Created by Andrey Sopov on 06.04.2025.
+//
+
---
+++ ImageFeed/Auth/OAuthTokenResponseBody.swift
@@ -0,0 +1,15 @@
+//
+//  struct OAuthTokenResponse.swift
+//  ImageFeed
+//
+//  Created by Andrey Sopov on 06.04.2025.
+//
+
+import Foundation
+
+public struct OAuthTokenResponse: Decodable {
+    var accessToken : String
+    var tokenType : String
+    var scope: String
+    var createdAt : Int
+}
