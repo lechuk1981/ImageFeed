3861b6bfb3182f32642ee83c1863436ba71c4f05
Lechuk <lechuk@yandex.ru>
2025-04-14 23:24:11 +0300

    Fixed fullscreen for AuthViewController and back button

--- ImageFeed/Splash/SplashViewController.swift
+++ ImageFeed/Splash/SplashViewController.swift
@@ -24,43 +24,45 @@ final class SplashViewController: UIViewController {
     }
     
     private func switchToTabBarController() {
-            guard let window = UIApplication.shared.windows.first else { fatalError("Invalid Configuration") }
-            let tabBarController = UIStoryboard(name: "Main", bundle: .main)
-                .instantiateViewController(withIdentifier: "TabBarViewController")
-            window.rootViewController = tabBarController
-        }
+        guard let window = UIApplication.shared.windows.first else { fatalError("Invalid Configuration") }
+        let tabBarController = UIStoryboard(name: "Main", bundle: .main)
+            .instantiateViewController(withIdentifier: "TabBarViewController")
+        window.rootViewController = tabBarController
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-          if segue.identifier == showAuthenticationSegue {
+        if segue.identifier == showAuthenticationSegue {
             guard
-              let navigationController = segue.destination as? UINavigationController,
-              let viewController = navigationController.viewControllers[0] as? AuthViewController else {
-              preconditionFailure("Error with \(showAuthenticationSegue)")
+                let navigationController = segue.destination as? UINavigationController,
+                let viewController = navigationController.viewControllers[0] as? AuthViewController else {
+                preconditionFailure("Error with \(showAuthenticationSegue)")
             }
+            viewController.navigationController?.modalPresentationStyle = .fullScreen
             viewController.delegate = self
-          } else {
+            
+        } else {
             super.prepare(for: segue, sender: sender)
-          }
         }
-
-        func fetchAuthToken(with code: String) {
-            oauth2Service.fetchAuthToken(with: code) { [weak self] result in
+    }
+    
+    func fetchAuthToken(with code: String) {
+        oauth2Service.fetchAuthToken(with: code) { [weak self] result in
             guard let self else { preconditionFailure("Cannot make weak link") }
             switch result {
             case .success(let result):
-              print("ITS LIT \(result)")
-              dismiss(animated: true)
-              self.switchToTabBarController()
+                print("ITS LIT \(result)")
+                dismiss(animated: true)
+                self.switchToTabBarController()
             case .failure(let error):
-              print("The error \(error)")
+                print("The error \(error)")
             }
-          }
         }
-      }
+    }
+}
 
-      extension SplashViewController: AuthViewControllerDelegate {
-        func authViewController(_ viewController: AuthViewController, didAuthenticateWithCode code: String) {
-          fetchAuthToken(with: code)
-        }
-      }
+extension SplashViewController: AuthViewControllerDelegate {
+    func authViewController(_ viewController: AuthViewController, didAuthenticateWithCode code: String) {
+        fetchAuthToken(with: code)
+    }
+}
 
