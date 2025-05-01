f93287edae8b7dcd9ed393adb1ba131d7f022be2
Lechuk <lechuk@yandex.ru>
2025-05-01 21:27:20 +0300

    Added ProfileService

---
+++ ImageFeed/Auth/ProfileService.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  ImageFeed
+//
+//  Created by Andrey Sopov on 01.05.2025.
+//
+
---
+++ ImageFeed/Profile/ProfileService.swift
@@ -0,0 +1,28 @@
+//
+//  Untitled.swift
+//  ImageFeed
+//
+//  Created by Andrey Sopov on 01.05.2025.
+//
+
+import  UIKit
+
+struct ProfileResult: Codable {
+    let username: String
+    let firstName: String?
+    let lastName: String?
+    let bio: String?
+}
+
+struct Profile {
+    let username: String
+    let name: String
+    let loginName: String
+    let bio: String?
+}
+
+final class ProfileService {
+    
+    
+    
+}
