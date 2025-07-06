38b1c0f816efdaa28c3595539c7b0b139bde0905
Lechuk <lechuk@yandex.ru>
2025-07-06 14:50:21 +0300

    Before testing

--- ImageFeed/ImageList/ImagesListService.swift
+++ ImageFeed/ImageList/ImagesListService.swift
@@ -9,29 +9,30 @@ import Foundation
 
 struct PhotoResult: Decodable {
     let id: String
-    let created: String?
+    let createdAt: String?
     //"updated_at": "2016-07-10T11:00:01-05:00",
     let width: Int
     let height: Int
     
-//    "color": "#60544D",
-//    "blur_hash": "LoC%a7IoIVxZ_NM|M{s:%hRjWAo0",
-//    "likes": 12,
+    //    "color": "#60544D",
+    //    "blur_hash": "LoC%a7IoIVxZ_NM|M{s:%hRjWAo0",
+    //    "likes": 12,
     let urls: UrlsResult
     let likedByUser: Bool
     let description: String?
-//    "user": {
-//      // ...
-//    },
+
+    //    "user": {
+    //      // ...
+    //    },
     enum CodingKeys: String, CodingKey {
-            case id, width, height, description, urls
-            case created = "created_at"
-            case likedByUser = "liked_by_user"
-        }
+        case id, width, height, description, urls
+        case createdAt = "created_at"
+        case likedByUser = "liked_by_user"
+    }
 }
 
 struct UrlsResult: Decodable {
-    let tumb: String
+    let thumb: String
     let full: String
 }
 
@@ -48,5 +49,58 @@ struct Photo {
 final class ImagesListService {
     static let didChangeNotification = Notification.Name("ImagesListServiceDidChange")
     private var photos: [Photo] = []
-
+    private var lastLoadedPage: Int?
+    private var loading = false
+    private var task: URLSessionTask?
+    private let urlSession = URLSession.shared
+    private static let dateFormatter = ISO8601DateFormatter()
+    
+    func fetchPhotosNextPage() {
+        guard !loading else { return }
+        loading = true
+        
+        let nextPage = (lastLoadedPage ?? 0) + 1
+        let urlString = "https://api.unsplash.com/photos?page=\(nextPage)&per_page=\(15)"
+        guard let url = URL(string: urlString) else {
+            loading = false
+            return
+        }
+        
+        var request = URLRequest(url: url)
+                request.setValue("Client-ID \(Constants.accessKey)", forHTTPHeaderField: "Authorization")
+        
+        let task = urlSession.dataTask(with: request) { [weak self] data, response, error in
+                    guard let self = self else { return }
+                    self.loading = false
+                    
+                    if let data = data {
+                        do {
+                            let decoder = JSONDecoder()
+                            let photoResults = try decoder.decode([PhotoResult].self, from: data)
+                            let newPhotos = photoResults.map { result in
+                                Photo(
+                                    id: result.id,
+                                    size: CGSize(width: result.width, height: result.height),
+                                    createdAt: Self.dateFormatter.date(from: result.createdAt ?? ""),
+                                    welcomeDescription: result.description,
+                                    thumbImageURL: result.urls.thumb,
+                                    largeImageURL: result.urls.full,
+                                    isLiked: result.likedByUser
+                                )
+                            }
+                            DispatchQueue.main.async {
+                                self.photos.append(contentsOf: newPhotos)
+                                self.lastLoadedPage = nextPage
+                                NotificationCenter.default.post(name: ImagesListService.didChangeNotification, object: self)
+                            }
+                        } catch {
+                            print("Decoding error: \(error)")
+                        }
+                    } else if let error = error {
+                        print("Loading error: \(error)")
+                    }
+                }
+                task.resume()
+    }
+    
 }
--- ImageFeed/ImageList/UrlsResult.swift
+++
@@ -1,7 +0,0 @@
-//
-//  UrlsResult.swift
-//  ImageFeed
-//
-//  Created by Andrey Sopov on 06.07.2025.
-//
-
