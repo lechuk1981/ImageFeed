04b3922a421f48324865be27a7cbe823f12b2e24
Lechuk <lechuk@yandex.ru>
2025-07-15 00:53:06 +0300

    Added WebViewTests

--- ImageFeed/Auth/WebViewPresenter.swift
+++ ImageFeed/Auth/WebViewPresenter.swift
@@ -23,25 +23,6 @@ final class WebViewPresenter: WebViewPresenterProtocol {
     }
     
     func viewDidLoad() {
-        //        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
-        //            return
-        //        }
-        //
-        //        urlComponents.queryItems = [
-        //            URLQueryItem(name: "client_id", value: Constants.accessKey),
-        //            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-        //            URLQueryItem(name: "response_type", value: "code"),
-        //            URLQueryItem(name: "scope", value: Constants.accessScope)
-        //        ]
-        //
-        //        guard let url = urlComponents.url else {
-        //            print("[WebViewViewController] Unable to build URL")
-        //            return
-        //        }
-        //
-        //        let request = URLRequest(url: url)
-        //        didUpdateProgressValue(0)
-        //        view?.load(request: request)
         guard let request = authHelper.authRequest() else { return }
         
         view?.load(request: request)
@@ -61,11 +42,6 @@ final class WebViewPresenter: WebViewPresenterProtocol {
     }
     
     func code(from url: URL) -> String? {
-//        guard let urlComponents = URLComponents(string: url.absoluteString),
-//              urlComponents.path == "/oauth/authorize/native",
-//              let codeItem = urlComponents.queryItems?.first(where: { $0.name == "code" })
-//        else { return nil}
-//        return codeItem.value
         authHelper.code(from: url)
     }
 }
---
+++ ImageFeed/Auth/WebViewViewControllerSpy.swift
@@ -0,0 +1,26 @@
+//
+//  WebViewViewControllerSpy.swift
+//  ImageFeed
+//
+//  Created by Andrey Sopov on 15.07.2025.
+//
+
+import Foundation
+
+final class WebViewViewControllerSpy: WebViewViewControllerProtocol {
+    var presenter: ImageFeed.WebViewPresenterProtocol?
+
+    var loadRequestCalled: Bool = false
+
+    func load(request: URLRequest) {
+        loadRequestCalled = true
+    }
+
+    func setProgressValue(_ newValue: Float) {
+
+    }
+
+    func setProgressHidden(_ isHidden: Bool) {
+
+    }
+}
